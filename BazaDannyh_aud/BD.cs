51b818b80a4205e76c798e2225b010cd3c2e187f
AnonPrimo <esichkaisi@gmail.com>
2018-09-14 21:18:17 +0300

    Добавьте файлы проекта.

---
+++ BazaDannyh_aud/BD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace BazaDannyh_aud
+{
+    public class BD
+    {
+        static string connect = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=Z:\StudentsFiles\RPZ\RPZ3\Чубіна\BD.mdf;Integrated Security=True;Connect Timeout=30";
+        
+        static SqlConnection con;
+
+        public BD()
+        {
+            con = new SqlConnection(connect);
+        }
+
+        public static void InsertUsers(string n, string p)
+        {
+                con = new SqlConnection(connect);
+                con.Open();
+
+                SqlCommand com = new SqlCommand("insert into [dbo].Users(name_user, pass) values('" + n + "'," + p + ")", con);
+
+                com.ExecuteNonQuery();
+            
+                con.Close();
+
+        }
+
+
+
+    }
+}
